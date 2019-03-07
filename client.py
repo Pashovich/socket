189c298906bdc1601aad4db06ac38d13899ba4a3
Pashovich <addons125@gmail.com>
2019-03-07 16:06:18 +0700

    working server

---
+++ client.py
@@ -0,0 +1,44 @@
+from socket import *
+import sys
+import threading
+
+#host = 'pavel.tom.ru'
+#host = '78.140.8.143'
+host = "localhost"
+port = 9091
+addr = (host,port)
+end = False
+login = ''
+def getMessages():
+    while True:
+        recv_data = tcp_socket.recv(1024)
+        if recv_data:
+            print(bytes.decode(recv_data))
+        else:
+            print("CLOSE CONNECTION")
+            end = True
+            tcp_socket.close()
+            break
+def sendMessages():
+    login = input()
+    tcp_socket.send(str.encode(login))
+    login+=' :'
+    while True:
+        getdata = input()
+        if getdata == 'exit':
+            tcp_socket.close()
+            end = True
+            sys.exit()
+        else:
+            tcp_socket.send(getdata.encode("UTF-8"))
+
+tcp_socket = socket(AF_INET, SOCK_STREAM)
+tcp_socket.connect(addr)
+
+t1 = threading.Thread(target=getMessages,daemon=False)
+t2 = threading.Thread(target=sendMessages,daemon=False)
+t1.start()
+t2.start()
+t1.join()
+t2.join()
+print('all?')
---
+++ server.py
@@ -0,0 +1,59 @@
+from socket import *
+import rsa
+import threading
+#данные сервера
+host = 'localhost'
+port = 9091
+address = (host,port)
+conn =[]
+adrr = []
+threads =[]
+lenght = 0
+
+
+def getConnects():
+    i = 0
+    while True:
+        connTemp,adrrTemp = tcp_socket.accept()
+        conn.append(connTemp)
+        adrr.append(adrrTemp)
+        print(adrrTemp)
+        login = bytes.decode(connTemp.recv(1024))
+        threads.append(threading.Thread(target=getMessages,args=[connTemp,adrrTemp,login,i]))
+        threads[i].start()
+        i+=1
+        
+def sendMessagesToAll(cur,tempmes):
+        for temp in conn:
+                if temp != cur:
+                        temp.send(tempmes)
+        #print('send',i,len(conn))
+        #for j in range(len(conn)):
+         #       if j!=i:
+          #              conn[j].send(tempmes)
+            
+def getMessages(cur,adr,login,i):
+        while True:
+                data = cur.recv(1024)
+                if not data:
+                        conn.pop(conn.index(cur))
+                        adrr.pop(adrr.index(adr))
+                        break
+                else:
+                        data = bytes.decode(data)
+                        message_ = '\n' + login+' :' + data 
+                        message_ = str.encode(message_)
+                        sendMessagesToAll(cur,message_)
+
+tcp_socket = socket(AF_INET, SOCK_STREAM)
+tcp_socket.bind(address)
+tcp_socket.listen()
+
+t1 = threading.Thread(target=getConnects)
+
+t1.start()
+t1.join()
+
+
+
+    
